62e691da9e1a825d027dd02a4765eeabb6857eb9
THENNARASU07 <ethennarasu29@gmail.com>
2025-05-10 18:31:23 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,20 @@
+import random
+
+quotes = [
+
+"Believe in yourself and all that you are.",
+
+"Keep going. Everything you need will come to you.",
+
+"The future depends on what you do today.",
+
+"Don't watch the clock; do what it does. Keep going.",
+
+"Push yourself, because no one else is going to do it for you."
+]
+
+print("\nRANDOM QUOTE GENERATOR\n")
+
+quote = random.choice(quotes)
+
+print(f" {quote} \n")
